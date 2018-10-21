585878ca48525f1cf8a80c53c75a795dc4298f78
Arne Westphal <arne@westarne.de>
2018-10-21 16:04:45 +0200

    feat/chromium: automatically download chromium

--- install.js
+++
@@ -1,8 +0,0 @@
-/**
- * Installation Script.
- */
-
-var fs = require('fs');
-
-console.log(os.platform(), process.env);
-fs.writeFileSync(__dirname + "/" + "install.out", os.platform() + "\r\n" + JSON.stringify(process.env, null, 2));
